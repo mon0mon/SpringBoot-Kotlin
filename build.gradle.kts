d47a7819e93c4d37b1a264a58b8df839729461c4
이민기 <mon0mon@naver.com>
2024-11-12 21:29:31 +0900

    🛠️ project - PostgreSQL Docker-Compose set up

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
