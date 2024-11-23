7999512fdb7ba1f199076a021224a870e9dd2774
이민기 <mon0mon@naver.com>
2024-11-23 17:46:06 +0900

    🧪 test - Testing Author Create Pt 4

--- src/test/kotlin/com/mon0mon/bookstorebackend/controllers/AuthorsControllerTest.kt
+++ src/test/kotlin/com/mon0mon/bookstorebackend/controllers/AuthorsControllerTest.kt
@@ -2,13 +2,17 @@ package com.mon0mon.bookstorebackend.controllers
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.mon0mon.bookstorebackend.domain.dto.AuthorDto
-import org.assertj.core.api.Assertions.assertThat
+import com.mon0mon.bookstorebackend.domain.entities.AuthorEntity
+import com.mon0mon.bookstorebackend.services.AuthorService
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.mockito.ArgumentMatchers.eq
+import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
@@ -17,8 +21,37 @@ import org.springframework.test.web.servlet.post
 @AutoConfigureMockMvc
 class AuthorsControllerTest @Autowired constructor(
     private val mockMvc: MockMvc,
+    @MockBean
+    private val authorService: AuthorService,
 ) {
     val objectMapper = ObjectMapper()
+
+    @Test
+    fun `test that create Author saves the Author`() {
+        mockMvc.post("/v1/authors") {
+            contentType = MediaType.APPLICATION_JSON
+            accept = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(
+                AuthorDto(
+                    id = null,
+                    name = "John Doe",
+                    age = 30,
+                    image = "author-image.jpeg",
+                    description = "some-description.jpeg"
+                )
+            )
+        }
+
+        val expected = AuthorEntity(
+            id = null,
+            name = "John Doe",
+            age = 30,
+            image = "author-image.jpeg",
+            description = "some-description.jpeg"
+        )
+
+        verify(authorService).save(eq(expected))
+    }
 
     @Test
     fun `test that create author returns a HTTP 201 status on a successful create`() {
