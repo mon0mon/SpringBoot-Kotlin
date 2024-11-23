f99cb94b2d07ea975e23e37e92e748087d78b51a
이민기 <mon0mon@naver.com>
2024-11-23 22:11:47 +0900

    🧪 test - Testing Author Create Pt 7

--- src/test/kotlin/com/mon0mon/bookstorebackend/TestDataUtil.kt
+++ src/test/kotlin/com/mon0mon/bookstorebackend/TestDataUtil.kt
@@ -1,10 +1,20 @@
 package com.mon0mon.bookstorebackend
 
 import com.mon0mon.bookstorebackend.domain.dto.AuthorDto
+import com.mon0mon.bookstorebackend.domain.entities.AuthorEntity
 
 fun testAuthorDtoA(id: Long? = null) =
     AuthorDto(
         id = null,
+        name = "John Doe",
+        age = 30,
+        description = "Some description",
+        image = "author-image.jpeg",
+    )
+
+fun testAuthorEntityA(id: Long? = null) =
+    AuthorEntity(
+        id = id,
         name = "John Doe",
         age = 30,
         description = "Some description",
---
+++ src/test/kotlin/com/mon0mon/bookstorebackend/services/impl/AuthorServiceImplTest.kt
@@ -0,0 +1,29 @@
+package com.mon0mon.bookstorebackend.services.impl
+
+import com.mon0mon.bookstorebackend.repositories.AuthorRepository
+import com.mon0mon.bookstorebackend.testAuthorEntityA
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+
+@SpringBootTest
+class AuthorServiceImplTest @Autowired constructor(
+    private val underTest: AuthorServiceImpl,
+    private val authorRepository: AuthorRepository,
+) {
+
+    @Test
+    fun `test that save persists the Author in the database`() {
+        val savedAuthor = underTest.save(testAuthorEntityA())
+        assertThat(savedAuthor.id).isNotNull()
+
+        val recalledAuthor = authorRepository.findByIdOrNull(savedAuthor.id!!)
+        assertThat(recalledAuthor).isNotNull()
+        assertThat(recalledAuthor!!).isEqualTo(
+            testAuthorEntityA(id = savedAuthor.id)
+        )
+    }
+}
