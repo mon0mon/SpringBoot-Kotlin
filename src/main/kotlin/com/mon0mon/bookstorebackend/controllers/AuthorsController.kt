56400963e565af90e11ccbed8def4156a0c87802
이민기 <mon0mon@naver.com>
2024-11-23 17:26:38 +0900

    🧪 test - Testing Author Create Pt 3

--- src/main/kotlin/com/mon0mon/bookstorebackend/controllers/AuthorsController.kt
+++ src/main/kotlin/com/mon0mon/bookstorebackend/controllers/AuthorsController.kt
@@ -1,10 +1,11 @@
 package com.mon0mon.bookstorebackend.controllers
 
 import com.mon0mon.bookstorebackend.domain.dto.AuthorDto
-import com.mon0mon.bookstorebackend.domain.entities.AuthorEntity
 import com.mon0mon.bookstorebackend.services.AuthorService
 import com.mon0mon.bookstorebackend.toAuthorDto
 import com.mon0mon.bookstorebackend.toAuthorEntity
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -15,8 +16,9 @@ class AuthorsController(
 ) {
 
     @PostMapping(path = ["/v1/authors"])
-    fun createAuthor(@RequestBody authorDto: AuthorDto): AuthorDto {
-        return authorService.save(authorDto.toAuthorEntity())
+    fun createAuthor(@RequestBody authorDto: AuthorDto): ResponseEntity<AuthorDto> {
+        val createdAuthor = authorService.save(authorDto.toAuthorEntity())
             .toAuthorDto()
+        return ResponseEntity(createdAuthor, HttpStatus.CREATED)
     }
 }
