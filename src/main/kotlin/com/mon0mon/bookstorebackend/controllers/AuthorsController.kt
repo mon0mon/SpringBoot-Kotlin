7f314d41254fd758c0a46b9e4c6db1d243a88cdd
이민기 <mon0mon@naver.com>
2024-11-23 16:23:24 +0900

    ✨ feat - Author create Pt 1

---
+++ src/main/kotlin/com/mon0mon/bookstorebackend/controllers/AuthorsController.kt
@@ -0,0 +1,15 @@
+package com.mon0mon.bookstorebackend.controllers
+
+import com.mon0mon.bookstorebackend.domain.Author
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AuthorsController {
+
+    @PostMapping(path = ["/v1/authros"])
+    fun createAuthor(@RequestBody author: Author?) {
+
+    }
+}
