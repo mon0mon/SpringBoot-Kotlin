af7cb3430b2d859bdc23d748c63b3607f7fee610
이민기 <mon0mon@naver.com>
2024-11-24 10:52:53 +0900

    🧪 test - Test Pollution

--- src/test/kotlin/com/mon0mon/bookstorebackend/services/impl/AuthorServiceImplTest.kt
+++ src/test/kotlin/com/mon0mon/bookstorebackend/services/impl/AuthorServiceImplTest.kt
@@ -12,8 +12,10 @@ import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
+@Transactional
 class AuthorServiceImplTest @Autowired constructor(
     private val underTest: AuthorServiceImpl,
     private val authorRepository: AuthorRepository,
